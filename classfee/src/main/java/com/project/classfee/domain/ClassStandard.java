a0a3a859a95dc40ee30794bab190682046df7f38
ang00 <zhang10092009@hotmail.com>
2019-07-08 18:20:54 +0800

    fix classFee

--- classfee/src/main/java/com/project/classfee/domain/ClassStandard.java
+++ classfee/src/main/java/com/project/classfee/domain/ClassStandard.java
@@ -48,8 +48,11 @@ public class ClassStandard extends Entitys implements Serializable {
     @Column(name = "subsidies_sum", columnDefinition = "VARCHAR(32) COMMENT '中心补贴总金额'")
     private int subsidiesSum;
 
+    @Column(name = "classFee", columnDefinition = "INT(11) COMMENT '每节课课时费'")
+    private int classFee;
 
-    public ClassStandard(String standardId, String createYear,String specialtyIds, int studentSum, int studentSubsidies, int subsidiesSum, String centerId) {
+
+    public ClassStandard(String standardId, String createYear,String specialtyIds, int studentSum, int studentSubsidies, int subsidiesSum, int classFee, String centerId) {
         super.setCenterAreaId(centerId);
         this.standardId = standardId;
         this.createYear = createYear;
@@ -57,5 +60,6 @@ public class ClassStandard extends Entitys implements Serializable {
         this.studentSum = studentSum;
         this.studentSubsidies = studentSubsidies;
         this.subsidiesSum = subsidiesSum;
+        this.classFee = classFee;
     }
 }
