2549f31ad9ef994fe2b45bea93b2f86f7fcabea0
zjw <you@example.com>
2019-08-05 17:25:14 +0800

    commit classfee 8-5

--- portal/src/main/java/com/project/portal/classfee/controller/ImportClassFeeController.java
+++ portal/src/main/java/com/project/portal/classfee/controller/ImportClassFeeController.java
@@ -1,31 +1,26 @@
 package com.project.portal.classfee.controller;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.StrUtil;
+
 import com.project.base.common.keyword.DefineCode;
 import com.project.base.exception.MyAssert;
+import com.project.classfee.domain.ClassFeeInfo;
+import com.project.classfee.service.ClassFeeInfoService;
 import com.project.portal.response.WebResult;
-import com.project.schoolroll.domain.excel.StudentImport;
-import com.project.schoolroll.service.impl.ExcelImpServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
-import static com.project.schoolroll.domain.excel.Dic.IMPORT_STUDENTS;
 
 /**
  * @Auther: zhangyy
@@ -39,13 +34,12 @@ import static com.project.schoolroll.domain.excel.Dic.IMPORT_STUDENTS;
 @Api(value = "导入接口", tags = {"导入相关数据接口"})
 @RequestMapping(path = "/import", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class ImportClassFeeController {
-    private final ExcelImpServiceImpl excelImpService;
-    private final RedisTemplate redisTemplate;
+    private final ClassFeeInfoService classFeeInfoService;
+
 
     @Autowired
-    public ImportClassFeeController(ExcelImpServiceImpl excelImpService, RedisTemplate redisTemplate) {
-        this.excelImpService = excelImpService;
-        this.redisTemplate = redisTemplate;
+    public ImportClassFeeController(ClassFeeInfoService classFeeInfoService) {
+        this.classFeeInfoService = classFeeInfoService;
     }
 
 //    @UserLoginToken
@@ -53,28 +47,26 @@ public class ImportClassFeeController {
     @PostMapping(path = "/classFee")
     @ApiImplicitParam(name = "file", value = "需要导入的Excel文件", required = true, paramType = "body", dataTypeClass = File.class)
     public WebResult leadingInStudents(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()){
-            MyAssert.isNull(null, DefineCode.ERR0010, "导入的文件不存在,请重新选择");
-        }
+        MyAssert.isTrue(file.isEmpty(), DefineCode.ERR0010, "导入的文件不存在,请重新选择");
+        //TODO 需要从公共的REDIS信息里面取值
+        String centerId="01";
         try {
-            if (redisTemplate.hasKey(IMPORT_STUDENTS)){
-                MyAssert.isNull(null, DefineCode.ERR0013, "有人操作，请稍后再试!");
+            List<ClassFeeInfo> list= classFeeInfoService.excelReader(file.getInputStream(), ClassFeeInfo.class);
+
+            //没有可导入的数据，删除键值，返回异常信息
+            if(list.size()==0){
+                classFeeInfoService.delExcelKey();
+                MyAssert.isTrue(true, DefineCode.ERR0010, "没有可导入的数据");
             }
-            //设置导入修改时间 防止失败没有过期时间
-            redisTemplate.opsForValue().set(IMPORT_STUDENTS, DateUtil.now());
-            redisTemplate.expire(IMPORT_STUDENTS, 30, TimeUnit.MINUTES);
-            String type = FileUtil.extName(file.getOriginalFilename());
-            if (StrUtil.isNotBlank(type) && "xlsx".equals(type)){
-                excelImpService.studentsExcel07Reader(file.getInputStream(), StudentImport.class);
-                return WebResult.okResult();
-            }else if (StrUtil.isNotBlank(type) && "xls".equals(type)){
-                excelImpService.studentsExcel03Reader(file.getInputStream(), StudentImport.class);
-                return WebResult.okResult();
-            }
+            //文件信息的数据库添加操作。
+            classFeeInfoService.impFile(list,list.get(0).getCreateYear(),list.get(0).getCreateMonth(),centerId) ;
+        return WebResult.okResult();
         } catch (IOException e) {
+            //导入错误，删除REDIS键值
+            classFeeInfoService.delExcelKey();
             log.error("students in IOException, file : [{}],  message : [{}]", file, e.getMessage());
             e.printStackTrace();
         }
-        return WebResult.failException("导入的文件格式不是Excel文件");
+        return WebResult.failException("导入的Excel文件数据错误");
     }
 }
