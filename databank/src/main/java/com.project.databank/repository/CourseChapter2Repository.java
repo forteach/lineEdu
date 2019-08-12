a12bdf7334222ff929d269a8cb937ae384b1dcd9
ang00 <zhang10092009@hotmail.com>
2019-08-12 11:59:54 +0800

    添加设置导入视频信息长度设置为章节观看视频的长度

---
+++ databank/src/main/java/com.project.databank/repository/CourseChapter2Repository.java
@@ -0,0 +1,14 @@
+package com.project.databank.repository;
+
+import com.project.databank.domain.CourseChapter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author: zhangyy
+ * @email: zhang10092009@hotmail.com
+ * @date: 19-8-11 18:00
+ * @version: 1.0
+ * @description:
+ */
+public interface CourseChapterRepository extends JpaRepository<CourseChapter, String> {
+}
