c897d3eaa7ea26787ede102e9a101a06650bb844
ang00 <zhang10092009@hotmail.com>
2019-08-14 11:57:04 +0800

    fix train model

--- train/src/main/java/com/project/train/domain/TrainProject.java
+++ train/src/main/java/com/project/train/domain/TrainProject.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 
