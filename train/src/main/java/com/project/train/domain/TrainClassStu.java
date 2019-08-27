f24e797b864a61a31fa852abd354a70ef6d8d1ce
ang00 <zhang10092009@hotmail.com>
2019-08-27 16:18:55 +0800

    fix nation

--- train/src/main/java/com/project/train/domain/TrainClassStu.java
+++ train/src/main/java/com/project/train/domain/TrainClassStu.java
@@ -53,8 +53,8 @@ public class TrainClassStu extends Entitys implements Serializable {
     @Column(name = "stu_name", columnDefinition = "VARCHAR(60) COMMENT '姓名'")
     private String stuName;
 
-    @Column(name = "marriage", columnDefinition = "VARCHAR(60) COMMENT '民族'")
-    private String marriage;
+    @Column(name = "nation", columnDefinition = "VARCHAR(60) COMMENT '民族'")
+    private String nation;
 
     @Column(name = "job_title", columnDefinition = "VARCHAR(60) COMMENT '单位职务'")
     private String jobTitle;
